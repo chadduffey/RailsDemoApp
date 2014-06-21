8176dd913e39e85979244650b7af2c21909ee7ca
chadduffey <chadduffey@hotmail.com>
2014-06-20 19:31:15 -0700

    new computer, syncing up

---
+++ app/models/listing.rb
@@ -0,0 +1,18 @@
+class Listing < ActiveRecord::Base
+  	if Rails.env.development?
+  		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "noimage.jpeg"
+  		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	else
+	  	has_attached_file 	:image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "noimage.jpeg",
+	  						:storage => :dropbox,
+	    					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	end
+
+	validates :name, :description, :price, presence: true
+	validates :price, numericality: { greater_than: 0}
+	validates_attachment_presence :image
+
+	belongs_to :user
+	has_many :orders
+end
